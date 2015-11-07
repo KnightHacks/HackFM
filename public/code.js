17aba2e2251e05f5a3f4a816475ea09148154193
ivey <iveysaurrr@gmail.com>
2015-11-07 03:06:02 -0500

    Clean up

--- public/code.js
+++ public/code.js
@@ -1,38 +1,27 @@
 var socket = io();
 var musicList = new Array();
+var player;
 
 socket.on('online', function (data) {
     console.log(data);
-    socket.emit('my other event', { my: 'data' });
     musicList = data;
     renderQueue();
   });
 
-setTimeout(function() {
-  console.log("emit");
-}, 5000);
-
-// 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
-
 function onYouTubePlayerAPIReady() {
 	updateQueue(musicList);
 }
 
-// 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
 	event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
 	if (event.data == YT.PlayerState.PLAYING && !done) {
